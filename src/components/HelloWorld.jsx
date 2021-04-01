6e266b671196ca20d3e43fb6a0cfff8c15c63b80
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-04-02 00:35:38 +0100

    estlint

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,8 @@
 import React  from 'react';
+import '../assets/styles/App.scss'
 
-const HelloWorld = () => {
+const HelloWorld = () => (
   <h1>Hola Mundo</h1>
-}
+)
 
 export default HelloWorld
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom'
 import HelloWorld from './components/HelloWorld'
 
-ReactDom.render( <HelloWorld/>, document.getElementById(app))
+ReactDom.render( <HelloWorld/>, document.getElementById("app"))
