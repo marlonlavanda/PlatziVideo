508e53ee4a890c5917f89e082796bd816f273bc3
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-04-05 23:16:34 +0100

    Implementacion de fake api

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -9,16 +9,37 @@ import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => {
+  const [videos, setVideos] = useState({
+    mylist: [],
+    trends: [],
+    originals: [],
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/initalState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
   return (
     <div className='App'>
       <Header />
       <Search />
+      { videos.mylist.length > 0 && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
 
-      <Categories>
+      <Categories title='Tendencias'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          { videos.trends?.map((item) => <CarouselItem key={item.id} {...item} />) }
+        </Carousel>
+      </Categories>
+
+      <Categories title='Originales de Platzi Video'>
+        <Carousel>
           <CarouselItem />
           <CarouselItem />
           <CarouselItem />
