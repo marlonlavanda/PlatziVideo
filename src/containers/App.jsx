d823e40decf191a9b3dc0e4e9f98c2546ca24a7f
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-04-06 23:15:40 +0100

    Implementación de hooks en el projecto

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,24 +7,17 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
+import useInitialState from '../hooks/useInitialState';
+
+const API = 'http://localhost:3000/initalState';
 
 const App = () => {
-  const [videos, setVideos] = useState({
-    mylist: [],
-    trends: [],
-    originals: [],
-  });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/initalState')
-      .then((response) => response.json())
-      .then((data) => setVideos(data));
-  }, []);
-  return (
+  const initalState = useInitialState(API);
+  return initalState.length === 0 ? <h1>Loading...</h1> : (
     <div className='App'>
       <Header />
       <Search />
-      { videos.mylist.length > 0 && (
+      { initalState.mylist.length > 0 && (
         <Categories title='Mi lista'>
           <Carousel>
             <CarouselItem />
@@ -34,7 +27,7 @@ const App = () => {
 
       <Categories title='Tendencias'>
         <Carousel>
-          { videos.trends?.map((item) => <CarouselItem key={item.id} {...item} />) }
+          { initalState.trends?.map((item) => <CarouselItem key={item.id} {...item} />) }
         </Carousel>
       </Categories>
 
