ccac0513f394bba327aaa6dc6bd1ee11100d2984
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-04-15 22:23:05 +0100

    añadido el componente register a la ruta

---
+++ src/containers/Register.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import '../assets/styles/components/Register.scss';
+
+const Register = () => {
+  return (
+    <section className='register'>
+      <section className='register__container'>
+        <h2>Regístrate</h2>
+        <form className='register__container–form'>
+          <input className='input' type='text' placeholder='Nombre' />
+          <input className='input' type='text' placeholder='Correo' />
+          <input className='input' type='password' placeholder='Contraseña' />
+          <button className='button' type='button'>Registrarme</button>
+        </form>
+        <a href='/'>Iniciar sesión</a>
+      </section>
+    </section>
+  );
+};
+
+export default Register;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../containers/Home';
+import Register from '../containers/Register';
 
 const App = () => (
   <BrowserRouter>
     <Route exact path='/' component={Home} />
+    <Route exact path='/register' component={Register} />
+
   </BrowserRouter>
 );
 
