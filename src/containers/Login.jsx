fbd4fbe755661c564468e837a373513414633659
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-04-17 00:00:01 +0100

    componente login y 404 añadidos

---
+++ src/containers/Login.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import GoogleIcon from '../assets/static/google-icon.png';
+import TwitterIcon from '../assets/static/twitter-icon.png';
+
+import '../assets/styles/components/Login.scss';
+
+const Login = () => {
+  return (
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Inicia sesión</h2>
+        <form className='login__container--form'>
+          <input className='input' type='text' placeholder='Correo' />
+          <input className='input' type='password' placeholder='Contraseña' />
+          <button className='button' type='button'>Iniciar sesión</button>
+          <div className='login__container--remember-me'>
+            <label>
+              <input type='checkbox' id='cbox1' value='first_checkbox' />
+              Recuérdame
+            </label>
+            <a href='/'>Olvidé mi contraseña</a>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div>
+            <img src={GoogleIcon} />
+            {' '}
+            Inicia sesión con Google
+          </div>
+          <div>
+            <img src={TwitterIcon} />
+            {' '}
+            Inicia sesión con Twitter
+          </div>
+        </section>
+        <p className='login__container--register'>
+          No tienes ninguna cuenta
+          {' '}
+          <a href='/register'>Regístrate</a>
+        </p>
+      </section>
+    </section>
+  );
+};
+
+export default Login;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../containers/Home';
 import Register from '../containers/Register';
+import Login from '../containers/Login';
+import NotFound from '../containers/NotFound';
 
 const App = () => (
   <BrowserRouter>
-    <Route exact path='/' component={Home} />
-    <Route exact path='/register' component={Register} />
-
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+      <Route component={NotFound} />
+    </Switch>
   </BrowserRouter>
 );
 
