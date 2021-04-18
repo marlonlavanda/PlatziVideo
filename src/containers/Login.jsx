779e9061c324e97b9811f00d100f3ff2ab99f698
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-04-18 23:04:53 +0100

    add favorite and remove favorite actions added

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import GoogleIcon from '../assets/static/google-icon.png';
@@ -7,14 +7,42 @@ import TwitterIcon from '../assets/static/twitter-icon.png';
 import '../assets/styles/components/Login.scss';
 
 const Login = () => {
+  const [form, setValues] = useState({
+    email: '',
+  });
+
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+  };
+
   return (
     <section className='login'>
       <section className='login__container'>
         <h2>Inicia sesión</h2>
-        <form className='login__container--form'>
-          <input className='input' type='text' placeholder='Correo' />
-          <input className='input' type='password' placeholder='Contraseña' />
-          <button className='button' type='button'>Iniciar sesión</button>
+        <form className='login__container--form' onSubmit={handleSubmit}>
+          <input
+            name='email'
+            className='input'
+            type='text'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            className='input'
+            type='password'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button className='button' type='submit'>Iniciar sesión</button>
           <div className='login__container--remember-me'>
             <label>
               <input type='checkbox' id='cbox1' value='first_checkbox' />
